9963414a8dc9cec337336b14c8027201b8c36689
IvannnZ <zhukov.ivan05@yandex.ru>
2024-02-17 15:57:29 +0700

    reviev chaenges

--- include/Matrix.h
+++ include/Matrix.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <cassert>
 #include <cmath>
 #include <cstddef>
 #include <initializer_list>
@@ -9,32 +10,36 @@ namespace hck2d2024 {
 
 
 class Matrix {
+private:
+    size_t rows_;
+    size_t columns_;
+    std::vector<double> elements_;
 
 public:
-
-Matrix(size_t rows, size_t columns){
-        rows_ = rows;
-        columns_ = columns;
-
-        *arr = new double[rows * columns];
+    Matrix(size_t rows, size_t columns) 
+    : rows_(rows), columns_(columns) {
+        elements_.resize(rows * columns);
     };
+    
 
-    ~Matrix(){
-        delete [] *arr;
+    double &operator[](size_t row, size_t column) {
+        return elements_[row * columns_ + column];
+    }
+
+    double operator[](size_t row, size_t column) const {
+        return elements_[row * columns_ + column];;
     }
     
-    double* operator[](size_t rows, size_t columns)const{
-        return arr[rows_ * rows + columns];
-    };
+    //Matrix matr(2,2) = [1,2,3,4];
 
 
-    bool operator==(Matrix matrix)const{
+    bool operator==(Matrix matrix) const {
         if (this->rows_ != matrix.rows_ || this->columns_ != matrix.columns_){
             return false;
         }
         for (int i = 0; i<rows_; i++){
             for (int j = 0; j<columns_; j++){
-                if (*matrix[i, j] != *(*this)[i,j]){
+                if (matrix[i, j] != (*this)[i,j]){
                     return false;
                 }
             }
@@ -42,23 +47,48 @@ Matrix(size_t rows, size_t columns){
         return true;
     };
 
-    Matrix operator*(Matrix mat)const;
+    Matrix operator*(Matrix matrix)const{
+        assert(this->rows_ != matrix.columns_);// разваливаемся же?
+        Matrix result_matrix(this->rows_, matrix.columns_);
+        for (int i = 0; i < this->rows_; i++) {
+            for (int j = 0; j < matrix.columns_; j++) {
+                *result_matrix[i,j] = 0;
+                for (int k = 0; k < matrix.rows_; k++) {
+                    *result_matrix[i,j] += *(*this)[i,k] * *matrix[k,j];
+                }
+            }
+        }
+        return result_matrix;
+    };
 
-    Matrix operator+(Matrix mat)const{};
+    Matrix operator+(Matrix matrix)const{
+        assert(this->rows_ != matrix.rows_ || this->columns_ != matrix.columns_);// разваливаемся же?
+        Matrix result_matrix(rows_, columns_);
+        for (int i = 0; i < rows_; i++){
+            for (int j = 0; j<columns_; j++){
+                *result_matrix[i,j] = *(*this)[i,j] + *matrix[i,j];
+            }
+        }
+        return result_matrix;
+    };
 
-    double determinant()const;
+    double det()const;
 
-    Matrix multiplyScalar (double num)const{
-        
-};
-
-    
-
-
-private:
-    size_t rows_;
-    size_t columns_;
-    double *arr[];
+    Matrix multiplyScalar (double lambda)const{
+        Matrix MatrixLambda = Matrix(this->rows_, this->columns_);
+        for(int i =0; i< rows_;i++){
+            for(int j = 0; j < columns_; i++ ){
+                *MatrixLambda[i,j] =  *(*this)[i,j] * lambda; 
+            }
+        }
+        return MatrixLambda;
+    };
+    size_t Rows() const{
+        return rows_;
+    };
+    size_t Columns_() const{
+        return rows_;
+    };
 
 
 };
